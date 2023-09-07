116de4ac32db6a9e28419ed45d9f994d8f910368
Prathamesh Powar <ppowar1910@gmail.com>
2023-09-07 19:23:46 +0530

    do small changes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-
+import ColorThief from 'colorthief';
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/",async (req, res) => {
   try {
